955995a95cf56b7cb3c45e0175e2b574411d1a18
EC2 Default User <ec2-user@ip-10-231-171-183.us-west-2.compute.internal>
2015-08-13 19:32:10 +0000

    GHC 7.6 compatibility

--- src/Data/Binary/C.hs
+++ src/Data/Binary/C.hs
@@ -6,12 +6,11 @@ import Data.Binary.Get
 import Data.Binary.Put
 import Data.ReinterpretCast (floatToWord, wordToFloat, doubleToWord, wordToDouble)
 import Foreign.C.Types
-import GHC.Prim (coerce)
 
 instance Binary CFloat where
-    get = coerce <$> wordToFloat <$> getWord32le
-    put = putWord32le . floatToWord . coerce
+    get = CFloat <$> wordToFloat <$> getWord32le
+    put = putWord32le . floatToWord . (let f (CFloat x) = x in f)
 
 instance Binary CDouble where
-    get = coerce <$> wordToDouble <$> getWord64le
-    put = putWord64le . doubleToWord . coerce
+    get = CDouble <$> wordToDouble <$> getWord64le
+    put = putWord64le . doubleToWord . (let f (CDouble x) = x in f)
